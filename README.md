5ac06b4dc237b90895ee21510a8c669859dd025e
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-01-18 00:28:13 +0600

    upload

---
+++ README.md
@@ -0,0 +1,19 @@
+## Hide Navbar on Scroll With Responsive Navbar
+
+---
+
+### Using Tools
+
+- HTML:5
+- Tailwind CSS
+- JavaScript
+
+### Using JavaScript
+
+- onclick event
+- onscroll event
+- window.scrollY
+
+### Live Website Link
+
+<a href='https://rejoyanislam.github.io/hide-navbar-on-scroll-with-responsive-navbar/'>Click</a>
---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+const menu = document.getElementById("menu");
+const menuBtn = document.getElementById("menuBtn");
+const topScroll = document.getElementById("topScroll");
+
+// menu toggle
+let result = false;
+menuBtn.onclick = () => {
+  if (!result) {
+    menu.classList.remove("hidden");
+    result = true;
+  } else {
+    menu.classList.add("hidden");
+    result = false;
+  }
+};
+
+// hide on scroll
+let beforeScroll = 0;
+window.onscroll = () => {
+  let newScroll = window.scrollY;
+  if (newScroll < beforeScroll) {
+ topScroll.classList.add("hidden");
+    beforeScroll = newScroll;
+  } else {
+    topScroll.classList.remove("hidden");
+   
+    beforeScroll = newScroll;
+  }
+};
+
+// bottom to stop by click button
+
+topScroll.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+    duration:"10s"
+  });
+  window.scrollY=0
+  console.log(window.scrollY);
+});
